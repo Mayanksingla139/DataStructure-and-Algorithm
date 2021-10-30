67bdea813d23dc56dcf5a2316cb28281ff83277b
Mayank Singla <55097726+Mayanksingla139@users.noreply.github.com>
2021-10-30 15:26:29 +0530

    Add files via upload

---
+++ Stack/ValidParentheses.cpp
@@ -0,0 +1,89 @@
+/*
+Input: s = "()"
+Output: true
+
+Input: s = "()[]{}"
+Output: true
+
+Input: s = "(]"
+Output: false
+
+Input: s = "([)]"
+Output: false
+
+Input: s = "{[]}"
+Output: true
+*/
+
+#include <iostream>
+#include <stack>
+using namespace std;
+
+bool valid(string s)
+{
+    stack<char> st;
+    bool ans = true;
+    for (int i = 0; i < s.length(); i++)
+    {
+        if (s[i] == '(' || s[i] == '[' || s[i] == '{')
+        {
+            st.push(s[i]);
+        }
+        else if (s[i] == ')')
+        {
+            if (!st.empty() && st.top() == '(')
+            {
+                st.pop();
+            }
+            else
+            {
+                ans = false;
+                break;
+            }
+        }
+        else if (s[i] == ']')
+        {
+            if (!st.empty() && st.top() == '[')
+            {
+                st.pop();
+            }
+            else
+            {
+                ans = false;
+                break;
+            }
+        }
+        else if (s[i] == '}')
+        {
+            if (!st.empty() && st.top() == '{')
+            {
+                st.pop();
+            }
+            else
+            {
+                ans = false;
+                break;
+            }
+        }
+    }
+    if (!st.empty())
+    {
+        return false;
+    }
+    return ans;
+}
+
+int main()
+{
+    string s;
+    cout << "enter the string:";
+    cin >> s;
+    if (valid(s))
+    {
+        cout << "True";
+    }
+    else
+    {
+        cout << "False";
+    }
+}
