ca6ee90cd334fb347e80373015c0d720fcb1e536
Mayank Singla <55097726+Mayanksingla139@users.noreply.github.com>
2022-01-21 21:28:07 +0530

    Add files via upload

---
+++ Array/FirstOccAndLastOcc.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+using namespace std;
+
+int FirstOcc(int arr[], int n, int k)
+{
+    int s = 0, e = n - 1;
+    int mid = s + (e - s) / 2;
+    int ans = -1;
+
+    while (s <= e)
+    {
+        if (arr[mid] == k)
+        {
+            ans = mid;
+            e = mid - 1;
+        }
+        else if (k > arr[mid])
+        {
+            s = mid + 1;
+        }
+        else
+        {
+            e = mid - 1;
+        }
+        mid = s + (e - s) / 2;
+    }
+
+    return ans;
+}
+
+int LastOcc(int arr[], int n, int k)
+{
+    int s = 0, e = n - 1;
+    int mid = s + (e - s) / 2;
+    int ans = -1;
+
+    while (s <= e)
+    {
+        if (arr[mid] == k)
+        {
+            ans = mid;
+            s = mid + 1;
+        }
+        else if (k > arr[mid])
+        {
+            s = mid + 1;
+        }
+        else
+        {
+            e = mid - 1;
+        }
+        mid = s + (e - s) / 2;
+    }
+
+    return ans;
+}
+
+int main()
+{
+    int a[6] = {1, 3, 3, 3, 5, 6};
+    int k = 3;
+    cout << "First Occurrence of 3 at index:" << FirstOcc(a, 6, k) << endl;
+    cout << "Last Occurrence of 3 at index:" << LastOcc(a, 6, k);
+}
