03eda71d581957c05c8b6e92391e3c61527634ba
Mayank Singla <55097726+Mayanksingla139@users.noreply.github.com>
2022-07-26 14:32:49 +0530

    Update Insertion_Sort.cpp

--- Searching & Sorting/Insertion_Sort.cpp
+++ Searching & Sorting/Insertion_Sort.cpp
@@ -1,6 +1,10 @@
 /*
 
-Insertion sort: Insertion sort is a sorting algorithm that places an unsorted element at its suitable place in each iteration. Insertion sort works similarly as we sort cards in our hand in a card game. We assume that the first card is already sorted then, we select an unsorted card. If the unsorted card is greater than the card in hand, it is placed on the right otherwise, to the left. In the same way, other unsorted cards are taken and put in their right place.
+Insertion sort: Insertion sort is a sorting algorithm that places an unsorted element at its suitable place in each iteration. 
+Insertion sort works similarly as we sort cards in our hand in a card game. 
+We assume that the first card is already sorted then, we select an unsorted card. 
+If the unsorted card is greater than the card in hand, it is placed on the right otherwise, to the left. 
+In the same way, other unsorted cards are taken and put in their right place.
 
 */
 
